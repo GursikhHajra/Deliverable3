fe9ce5bf136a93a626e4eb62c2ee3d5cbe7845eb
Peter-Mascherin <mascherp@sheridancollege.ca>
2020-04-18 14:14:08 -0400

    Added match loop, win conditions, point system, and more error handling

--- src/deliverable3/Game.java
+++ src/deliverable3/Game.java
@@ -27,6 +27,11 @@ public class Game {
         String playeronecard = "";
         String playertwocard = "";
         String drawcheck[] = new String[2];
+        boolean playagain = false;
+        
+        System.out.println("Welcome to War\nThis final Deliverable is a game of War between 2 people, first to 10 points wins!\nI hope you enjoy and have fun!\n");
+        do
+        {
         System.out.println("Please enter the PlayerId of player1");
         players[0].setPlayerId(scan.nextLine());
         do
@@ -38,7 +43,15 @@ public class Game {
          else
             break;
         }while(players[0].getPlayerId().equals(players[1].getPlayerId()));
+        int playeronepoints = 0;
+        int playertwopoints = 0;
+        int round = 1;
+        boolean playerwinlimit = false;
+        String playcheck = "";
         
+        
+        do
+        {
         
         do
         {
@@ -50,24 +63,67 @@ public class Game {
                 System.out.println("Please enter 'draw' exactly as it is, no other way");
         }while(!(drawcheck[0].equals("draw")) || !(drawcheck[1].equals("draw")));
         
-        
-        playeronecard = drawCard(1);
-        playertwocard = drawCard(2);
         do
         {
+            System.out.println("Round " + round + "!");
+        playeronecard = drawCard(1);
+        playertwocard = drawCard(2);
+        
+        
         System.out.println(players[0].getPlayerId() + " drew " + playeronecard);
         System.out.println(players[1].getPlayerId() + " drew " + playertwocard);
         if(numVal[0] > numVal[1])
+        {
             System.out.println(players[0].getPlayerId() + " wins!");
+            playeronepoints = playeronepoints + 1;
+            if(playeronepoints == 10)
+            {
+                playerwinlimit = true;
+            }
+        }
         else if(numVal[0] < numVal[1])
+        {
             System.out.println(players[1].getPlayerId() + " wins!");
+            playertwopoints =  playertwopoints + 1;
+            if(playertwopoints == 10)
+            {
+                playerwinlimit = true;
+            }
+        }
         else
         {
             System.out.println("Theres been a tie!");
-            playeronecard = drawCard(1);
-            playertwocard = drawCard(2);
         }
         }while(numVal[0] == numVal[1]);
+        round = round + 1;
+        System.out.println(players[0].getPlayerId() + "'s points currently are: " + playeronepoints);
+        System.out.println(players[1].getPlayerId() + "'s points currently are: " + playertwopoints);
+        }while(!playerwinlimit);
+        
+        if(playeronepoints == 10)
+            System.out.println(players[0].getPlayerId() + " wins this match of War!");
+        else if (playertwopoints == 10)
+            System.out.println(players[1].getPlayerId() + " wins this match of War!");
+        
+        do
+        {
+        System.out.println("Would you like to play again?\nEnter 'yes' to play again and 'no' to stop");
+        playcheck = scan.nextLine();
+        if(playcheck.equals("yes"))
+        {
+            playagain = true;
+            break;
+        }
+        else if(playcheck.equals("no"))
+        {
+            playagain = false;
+            break;
+        }
+        else
+            System.out.println("Please enter 'yes' or 'no' exactly as it is");
+        }while(!(playcheck.equals("yes")) || !(playcheck.equals("no")));
+        
+         }while(playagain);
         System.out.println("Thanks for Playing");
         
     }
@@ -78,13 +134,11 @@ public class Game {
         {
         numVal[0] = (int) (Math.random() * GroupOfCards.CardValue.values().length);
         numSuit[0] = (int) (Math.random() * GroupOfCards.CardSuit.values().length);
-        System.out.println("Player1 " +numVal[0]);
         }
         if(num == 2)
         {
         numVal[1] = (int) (Math.random() * GroupOfCards.CardValue.values().length);
         numSuit[1] = (int) (Math.random() * GroupOfCards.CardSuit.values().length);
-        System.out.println("Player2 " +numVal[1]);
         }
         
      
