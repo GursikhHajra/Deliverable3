7c7ca59d0f334d7233eb4d4c077e96488e00b663
latawan <59924389+latawan@users.noreply.github.com>
2020-04-19 00:43:09 -0400

    Update GameTest.java

--- test/deliverable3/GameTest.java
+++ test/deliverable3/GameTest.java
@@ -27,25 +27,45 @@ public class GameTest {
      */
     @Test
     public void testDrawCard() {
-        System.out.println("drawCard");
+        System.out.println("drawCardGood");
         int num = 0;
         String expResult = "ACE of DIAMONDS";
         String result = Game.drawCard(num);
         do{
+            
+            
             num++;
             if(num == 25){
                 num = 0;
             }
-          
+            
             result = Game.drawCard(num);
             
         }while(!expResult.equals(result));
-        assertNotEquals(null,result);
+        
         assertEquals(expResult,result);
+        
     }
     
+    @Test 
+    public void testDrawCard1() {
+        System.out.println("drawCardBad");
+        int num = 0;
+        String expResult = "";
+        String result = Game.drawCard(num);
+        
+        assertTrue(result.contains(expResult));
+        
+        
+        
+    }
     
-    
-    
+    public void testDrawCard2()
+    {
+        System.out.println("Boundary check");
+        int num = 0;
+        String result = Game.drawCard(num);
+        assertTrue((result.length() <= 17) && (result.length() >= 0));
+    }
     
 }
